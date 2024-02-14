e9aadcd15b8cee8025a8afb297f792fe869cc59b
hongminyeong <minyung1240@daum.net>
2024-02-14 16:58:05 +0900

    study: 1이 될때까지 두번쨰 풀이

---
+++ greedy/3-2.py
@@ -0,0 +1,16 @@
+N, K = map(int,input().split())
+
+count = 0
+
+while True:
+    target = (N//K)*K # 나눌수 있는 수
+    count += (N-target) #1 빼는횟수
+    N = target
+    if N < K:
+        break
+    #k로 나누기
+    count += 1
+    N //= K
+
+count += (N - 1) # 마지막으로 남은 수에 대해 1씩뺴기
+print(count)
